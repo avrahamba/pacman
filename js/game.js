fd90c8d2aec51d8070db2f52dbf61637ad2394b2
avraham <b.a.avraham@gmail.com>
2020-02-03 22:42:27 +0200

    fixed

--- js/game.js
+++ js/game.js
@@ -15,6 +15,12 @@ var gGame = {
 };
 
 function init() {
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
     var elEndGame = document.querySelector('.end-game');
     elEndGame.innerHTML = '';
     gBoard = buildBoard();
